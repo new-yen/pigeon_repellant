eaa7ec6fbfeca07e21302b6d72863f3cf403028d
David Nguyen <davidnguyen@Davids-MacBook-Pro.local>
2020-05-17 10:18:06 +0200

    servo angle 0

--- code.py
+++ code.py
@@ -11,14 +11,13 @@ PIR_PIN = board.D2                                  # Pin number connected to PI
 pir = digitalio.DigitalInOut(PIR_PIN)
 pir.direction = digitalio.Direction.INPUT
 
-
 # create a PWMOut object on Pin A3.
 pwm = pulseio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
-my_servo.angle = 0
 
+my_servo.angle = 0
 count = 0
 scan = True
 
@@ -27,7 +26,7 @@ while True:
     while scan:
         print("scanning")
         if pir.value:                               # PIR is detecting movement!
-            if count < 5:
+            if count < 25:
                 count += 1                          # Count +1
 
             else:
@@ -36,13 +35,13 @@ while True:
                 # cpx.play_file("bird_caw1.wav")
                 time.sleep(1)
                 cpx.pixels[0] = (0, 0, 0)
-                for angle in range(0, 180, 15):     # 0 - 180 degrees, 15 degrees at a time.
+                for angle in range(0, 180, 10):     # 0 - 180 degrees, 15 degrees at a time.
                     my_servo.angle = angle
-                    time.sleep(0.01)
+                    time.sleep(0.02)
                 time.sleep(2)
-                for angle in range(180, 0, -15):    # 180 - 0 degrees, 15 degrees at a time.
+                for angle in range(180, 0, -10):    # 180 - 0 degrees, 15 degrees at a time.
                     my_servo.angle = angle
-                    time.sleep(0.01)
+                    time.sleep(0.02)
                 count = 0                           
             scan = False
 
@@ -57,4 +56,4 @@ while True:
         scan = True
 
     print(count)
-    time.sleep(1.5)
+    time.sleep(0.25)
